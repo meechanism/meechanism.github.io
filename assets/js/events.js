347007423311bd073c7f3864ae5e6bda308ab261
Mee Cha <mee.cha.nism@gmail.com>
2018-06-11 14:09:31 -0700

    Fix spacing

--- assets/js/events.js
+++ assets/js/events.js
@@ -10,19 +10,6 @@ function ready(fn) {
   }
 }
 
-if (window) {
-  window.dataLayer = window.dataLayer || [];
-
-  window.Mee = window.Mee || {};
-  window.Mee.ga = function() {
-    dataLayer.push(arguments);
-  };
-
-  window.Mee.gaButtonClick = function(action, label) {
-    window.Mee.ga(action, "button_click", label || action);
-  };
-}
-
 ready(function() {
   const allEventEls = document.getElementsByClassName("ga-event");
   for (let i = 0; i < allEventEls.length; i++) {
@@ -30,8 +17,12 @@ ready(function() {
     const actionName = element.dataset.gaAction;
     const flavor = element.dataset.flavor;
     element.addEventListener("click", function(e) {
-      e.preventDefault();
-      window.Mee.gaButtonClick(actionName, flavor);
+      ga("send", {
+        hitType: "event",
+        eventCategory: "Link",
+        eventAction: actionName,
+        eventLabel: flavor
+      });
     });
   }
 });
